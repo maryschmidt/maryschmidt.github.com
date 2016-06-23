f66f994bcb60a9b53c9d81074867d2a39c9cdddc
Mary Schmidt <marykateschmidt@gmail.com>
2016-06-22 22:19:22 -0400

    Style update for homepage

--- _site/js/portfolio.js
+++ _site/js/portfolio.js
@@ -1,5 +1,22 @@
 $(document).ready(function() {
 
+  $('label').on('click', function(event) {
+    var label = $("label[for='" + event.target.id + "']");
+    $(label).hide();
+  });
+
+  $('input, textarea').on('focus', function(event) {
+    var label = $("label[for='" + event.target.name + "']");
+    $(label).hide();
+  });
+
+  $('input, textarea').on('blur', function(event) {
+    var label = $("label[for='" + event.target.name + "']");
+    if (event.target.value === '') {
+      $(label).show();
+    }
+  });
+
   $('.trigger').click(function() {
     scrollVal = $('#featured-projects').offset().top - 136;
 
